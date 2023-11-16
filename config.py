19dd4602d1c420831927c317fee232e9e59b3cf1
Connor-Prime <connorprime23@gmail.com>
2023-11-15 20:17:57 -0800

    added user setup

--- config.py
+++ config.py
@@ -28,3 +28,4 @@ class Config():
     FLASK_ENV = os.environ.get('FLASK_ENV') 
     FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'TopSneaky'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
--- rangers_shop/__init__.py
+++ rangers_shop/__init__.py
@@ -1,19 +1,30 @@
 from flask import Flask
+from flask_migrate import Migrate
+
+
 from .blueprints.site.routes import site #add this import to grab out site blueprint
 from .blueprints.about_page.routes import about_page
+from .blueprints.auth.routes import auth
 from config import Config
+from .models import login_manager, db 
+
+
 #instantiating our Flask app
 app = Flask(__name__) #passing in the __name__ variable which just takes the name of the folder we're in
 
 app.config.from_object(Config)
 #we are going to use a decorator to create our first route
-#@app.route("/")
-#def hello_world():
-#    return "<p>Hello World!</p>"
-
-# We can also comment out the above because this is rendering to the same
-# location as our site blueprint. 
+login_manager.init_app(app)
+login_manager.login_view = 'auth.sign_in' 
+login_manager.login_message = "Hey you! Log in please!"
+login_manager.login_message_category = 'warning'
 
 
-app.register_blueprint(site) # add this here to register your site blueprint
+app.register_blueprint(site) 
 app.register_blueprint(about_page) 
+app.register_blueprint(auth)
+
+#ADD THIS
+#instantiating our datbase & wrapping our app
+db.init_app(app)
+migrate = Migrate(app, db)
